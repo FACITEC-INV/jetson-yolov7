3c678b4f5456ebeee3c2063fcb9f8f5babbef040
NazarioLuis <nazarioluisaf@gmail.com>
2024-05-21 11:00:17 -0400

    Update send_data.py

--- send_data.py
+++ send_data.py
@@ -1,14 +1,15 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 from db import Detection
-import requests
+import httpx
 from datetime import datetime, timedelta
 
 urlBase = "#"
 
 def send():
     query = Detection.select()
-    response = requests.post(f'{urlBase}detections/getLastTransaction')
-    if response.ok:
+    response = httpx.post(f'{urlBase}detections/getLastTransaction', timeout=10.0)
+    if response.status_code == httpx.codes.OK:
+        print(response.json()['fecha'])
         lastTransaction = datetime.strptime(f"{response.json()['fecha']}:999999", '%Y-%m-%d %H:%M:%S:%f')
         nowStr = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         now = datetime.strptime(f"{nowStr}:000000", '%Y-%m-%d %H:%M:%S:%f')
@@ -23,8 +24,31 @@ def send():
                 "fecha": det.fecha.strftime('%Y-%m-%d %H:%M:%S')
             } for det in query] 
         }
-        response = requests.post(f'{urlBase}detections/add', json=data) 
+        print(data)
+        response = httpx.post(f'{urlBase}detections/add', json=data, timeout=10.0) 
+        #print(response.json())
+    else:
+        first()
     
+    
+def first():
+    query = Detection.select()
+    if True:
+        nowStr = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        now = datetime.strptime(f"{nowStr}:000000", '%Y-%m-%d %H:%M:%S:%f')
+        query = Detection.select().where(
+            Detection.fecha < now
+        )
+        data = { 
+            "detections": [{
+                "id_zona": det.zona,
+                "clase": det.clase.lower(),
+                "fecha": det.fecha.strftime('%Y-%m-%d %H:%M:%S')
+            } for det in query] 
+        }
+        response = httpx.post(f'{urlBase}detections/add', json=data, timeout=10.0) 
+
+
 def start(min):
     scheduler = BackgroundScheduler()
     scheduler.add_job(send, 'interval', minutes=min)
