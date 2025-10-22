9244af1d4f3b478171b0f90691081ade8a9d0bc6
nazario luis ayala <nazarioluisaf@gmail.com>
2025-10-22 08:49:01 -0300

    Refactor send_data.py to remove unused transaction retrieval logic and clean up code

--- send_data.py
+++ send_data.py
@@ -26,19 +26,8 @@ def send():
         url = endpoint["url"]
 
         try:
-            # Obtener la última transacción de este endpoints
-            response = httpx.post(f'{url}detections/getLastTransaction', timeout=10.0)
-            if response.status_code != httpx.codes.OK:
-                print(f"Fallo al obtener última transacción de {url}")
-                first(endpoint)
-                continue
-
-            print(f"Última transacción desde {url}: {response.json()['fecha']}")
-            lastTransaction = datetime.strptime(f"{response.json()['fecha']}:000000", '%Y-%m-%d %H:%M:%S:%f')
-
-            # Seleccionar los registros en el rango de tiempo, que no fueron enviados a este endpoint
+            # Seleccionar los registros que no fueron enviados a este endpoint
             query = Detection.select().where(
-                #(Detection.fecha >= (lastTransaction - timedelta(minutes=1))) &
                 (Detection.fecha < nowParsed) &
                 ~(Detection.enviado_a.contains(eid))
             ).order_by(Detection.fecha).limit(3000)
@@ -72,33 +61,6 @@ def send():
             print(f"Error en el procesamiento del endpoint {url}: {e}")
 
     cleanup_old_detections()
-    
-    
-def first(endpoint):
-    url = endpoint["url"]
-    eid = endpoint["id"]
-    nowStr = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    now = datetime.strptime(f"{nowStr}:000000", '%Y-%m-%d %H:%M:%S:%f')
-    query = Detection.select().where(
-        Detection.fecha < now
-    ).order_by(Detection.fecha).limit(3000)
-    data = {
-        "detections": [{
-            "id_zona": det.zona,
-            "clase": det.clase.lower(),
-            "fecha": det.fecha.strftime('%Y-%m-%d %H:%M:%S'),
-            "id_camara": ID_CAMARA,
-        } for det in query]
-    }
-    response = httpx.post(f'{url}detections/add', json=data, timeout=10.0)
-    print(f"first() response from {url}: {response.status_code}, {response.json() if response.status_code == 200 else ''}")
-    if response.status_code == httpx.codes.OK:
-        for det in query:
-            ya_enviados = det.enviado_a.split(",") if det.enviado_a else []
-            if eid not in ya_enviados:
-                ya_enviados.append(eid)
-                det.enviado_a = ",".join(ya_enviados)
-            det.save()
 
 def cleanup_old_detections():
     limite = datetime.now() - timedelta(days=30)
@@ -111,7 +73,6 @@ def cleanup_old_detections():
     print(delete_query.sql())
     deleted_count = delete_query.execute()
     print(f"Se eliminaron {deleted_count} registros enviados con más de 30 días.")
-    
 
 def start(min):
     scheduler = BackgroundScheduler()
